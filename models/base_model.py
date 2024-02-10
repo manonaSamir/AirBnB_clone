ddcce16f9e734ece52934aed149c614763879f94
manonaSamir <bena.yalla@yahoo.com>
2024-02-10 12:41:10 +0200

    fix imports

--- models/base_model.py
+++ models/base_model.py
@@ -5,7 +5,7 @@ This module for defines all common attributes/methods for other classes
 """
 
 import datetime
-from models import storage
+import models
 import uuid
 
 
@@ -17,7 +17,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.datetime.now()
             self.updated_at = datetime.datetime.now()
-            storage.new(self)
+            models.storage.new(self)
 
         else:
             for key, val in kwargs.items():
@@ -38,7 +38,7 @@ class BaseModel:
         """ updates the public instance attribute
        updated_at with the current datetime """
         self.updated_at = datetime.datetime.now()
-        storage.save()
+        models.storage.save()
 
     def to_dict(self):
         """returns a dictionary containing
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,7 +10,7 @@ from models.city import City
 from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
-
+import os
 
 class FileStorage:
     """serializes instances to a JSON file
